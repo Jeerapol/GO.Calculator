942c3b155697f11c5d1dd2e94046d822fe44a367
Nyagi337 <Jeerapol.Dev@gmail.com>
2024-11-17 20:35:10 +0700

    Add forloop, Adjust logic, Delete Generic Type

--- main.go
+++ main.go
@@ -1,82 +1,101 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
-type Number interface {
-    ~int | ~float32
-}
+
 
 func main() {
-	var number int
+	for {
 
+		showMenu()
+		choice := getChoice()
+		switch choice{
+		case 1,2,3,4:
+			calculator(choice)
+		case 5:
+			fmt.Println("Thanks you for using claculator")
+			os.Exit(0)
+		default:
+			fmt.Println("Invalid input! Please try again")
+		}
+			
+	}
+}
+
+
+func showMenu(){
 	fmt.Println("Calculator")
 	fmt.Println("1. App")
 	fmt.Println("2. Minus")
 	fmt.Println("3. Multiply")
 	fmt.Println("4. Divided")
-	fmt.Print("Enter your choice (1-4): ")
-	fmt.Scanln(&number)
+	fmt.Println("5. Exit")
+}
 
-	switch number{
-	case 1:
-		var num1, num2 float32
-		fmt.Println("Add")
-		fmt.Print("Enter Number 1: ")
-		fmt.Scanln(&num1)
-		fmt.Print("Enter Number 2: ")
-		fmt.Scanln(&num2)
-		result := add(num1,num2)
-		fmt.Printf("%.2f + %.2f = %.2f",num1,num2,result)
-	case 2:
-		var num1, num2 float32
-		fmt.Println("Minus")
-		fmt.Print("Enter Number 1: ")
-		fmt.Scanln(&num1)
-		fmt.Print("Enter Number 2: ")
-		fmt.Scanln(&num2)
-		result := minus(num1,num2)
-		fmt.Printf("%.2f - %.2f = %.2f",num1,num2,result)
-	case 3:
-		var num1, num2 float32
-		fmt.Println("Multiply")
-		fmt.Print("Enter Number 1: ")
-		fmt.Scanln(&num1)
-		fmt.Print("Enter Number 2: ")
-		fmt.Scanln(&num2)
-		result := multiply(num1,num2)
-		fmt.Printf("%.2f x %.2f = %.2f",num1,num2,result)
-	case 4:
-		fmt.Println("Divided")
-		var num1, num2 float32
-		fmt.Print("Enter Number 1: ")
-		fmt.Scanln(&num1)
-		fmt.Print("Enter Number 2: ")
-		fmt.Scanln(&num2)
-		if result, err := divided(num1,num2); err == nil {
-			fmt.Printf("%.2f ÷ %.2f = %.2f",num1,num2,result)
+func getChoice() int {
+	var choice int
+	fmt.Print("Enter your choice (1-5): ")
+	fmt.Scanln(&choice)
+	return choice
+}
+
+func getNumbers() (float64, float64) {
+	var num1, num2 float64
+    fmt.Print("Enter Number 1: ")
+    fmt.Scanln(&num1)
+    fmt.Print("Enter Number 2: ")
+    fmt.Scanln(&num2)
+    return num1, num2 
+}
+
+func calculator(choice int){
+	num1, num2 := getNumbers()
+	var result float64
+	var err error
+	var operator string
+
+	switch choice {
+		case 1:
+			result = add(num1, num2)
+			operator = "+"
+		case 2:
+			result = minus(num1, num2)
+			operator = "-"
+		case 3:
+			result = multiply(num1, num2)
+			operator = "x"
+		case 4:
+			result,err = divide(num1, num2)
+			operator = "÷"
+			if err != nil {
+				fmt.Println("Error:", err)
+				return
+			}
 		}
-	default:
-		fmt.Println("Invalid input!")
-}
+		fmt.Printf("Result: %.2f %s %.2f = %.2f\n", num1, operator, num2, result)
+		fmt.Println("===========================")
 }
 
-func add[T Number](num1,num2 T) T {
+func add(num1, num2 float64) float64 {
 	return num1 + num2
 }
 
-func minus[T Number](num1,num2 T)T {
+func minus(num1, num2 float64) float64 {
 	return num1 - num2
 
 }
 
-func multiply[T Number](num1,num2 T)T {
+func multiply(num1, num2 float64) float64 {
 	return num1 * num2
 
 }
 
-func divided[T Number](num1, num2 T) (T, error) {
-    if num2 != 0 {
-        return num1 / num2, nil
-    }
-    return 0, fmt.Errorf("cannot divide by zero")
+func divide(num1, num2 float64)(float64,error){
+	if num2 == 0 {
+		return 0, fmt.Errorf("cannot divide by zero")
+	}
+	return num1 / num2, nil
 }
